178dcfb9a8146add5f4011ef950dfde8591f1b69
bielsoaresaraujo <126730273+bielsoaresaraujo@users.noreply.github.com>
2024-10-23 01:01:32 +0000

    atualizar

--- index.php
+++ index.php
@@ -13,29 +13,29 @@
         <lebel>Numero 02</lebel>
         <input type="text" name="numero2" id="numero2"/>
 
-        <button onclick="Calculara();">Calculara com ajax</button>
+        <button onclick="calcular();">Calculara com ajax</button>
         <p id="reultado"></p>
     </pre>
   <script>
     function calcular(){
-        const numero1 = document.getElementByid('numero01').value;
-         const numero2 = document.getElementByid('numero02').value;
+        const numero1 = document.getElementById("numero01").value;
+         const numero2 = document.getElementById("numero02").value;
          fetch( '/calculo.php',{
             method: 'POST',
             headers:{'Content-Type': 'application/json'},
             body: JSON.stringify({
-                numero1: perseFloat(numero1),
-                numero1: perseFloat(numero2)
+                numero1: parseFloat(numero1),
+                numero1: parseFloat(numero2)
             })
          })
          .then(resposta => resposta.json())
          .then(dados =>{
             
-            document.getElementByid("resultado").innerHTML =
-            "soma: " -+ dados.soma;
+            document.getElementById("resultado").innerHTML =
+            "soma: " + dados.soma;
          })
          .catch(erro =>{
-            document.getElementByid("resultado").innerHTML =
+            document.getElementByiId("resultado").innerHTML =
             "Erro ao processar";
          })
     }
